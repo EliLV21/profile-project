04dacdf7374bbae61596cc10900569e3febf4fbf
mlaguna <mlaguna@expediagroups.com>
2025-01-10 11:04:33 -0600

    save router change

--- src/app/components/shared/nav/nav.tsx
+++ src/app/components/shared/nav/nav.tsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>Board Page</h1>
+    <div className="grid grid-cols-4 w-full h-[4rem] flex justify-content items-center">
+      <nav className="col-span-3 inline-block">
+        <ul className="nav flex flex-row">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item ml-[1.25rem]">
+            <Link className="nav-link" to="/board">
+              Board
+            </Link>
+          </li>
+          {/* <li className="nav-item ml-[1.25rem]">
+            <Link className="nav-link" to="/carousel">
+              Carousel
+            </Link>
+          </li>
+          <li className="nav-item ml-[1.25rem]">
+            <Link className="nav-link" to="/notes">
+              Notes
+            </Link>
+          </li> */}
+        </ul>
+      </nav>
+      <div className="flex justify-center items-center">{/* <Login /> */}</div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Suspense, useState } from 'react';
 import NavBar from './components/shared/nav/nav';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HomePage } from './components/pages/home/home';
 import BoardPage from './components/pages/board/page';
@@ -31,8 +31,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <QueryClientProvider client={queryClient}>
         <UserNameContext.Provider value={defectValue}>
-          <NavBar />
           <Router future={{ v7_startTransition: true }}>
+            <NavBar />
+
             <Suspense fallback={<div>Loading...</div>}>
               <Routes>
                 <Route path="/" element={<HomePage />} />
