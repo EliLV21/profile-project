1d2db2c4cd0138562f58ada9c9a8555b80273326
EliLV21 <miriam.elizabeth.lv@gmail.com>
2025-01-16 14:29:43 -0600

    A 5 (#15)
    Co-authored-by: mlaguna <mlaguna@expediagroups.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,7 @@ export default function Home() {
   const { name } = useUserContext();
 
   const isProd = process.env.NODE_ENV === 'production' && process.env.DEPLOY_ENV === 'github-pages';
-
-  console.log('isProd', isProd, process.env);
-
+  console.log('isProd', isProd, process.env.NODE_ENV);
 
   const [defectValue, setDefectValue] = useState<DefectValue>({
     name: '',
